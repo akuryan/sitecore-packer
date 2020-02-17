83b4685a903fcd834a2a79470458d51b4ad1b124
Anton Kuryan <Anton_Kuryan@epam.com>
2020-02-17 14:00:38 +0300

    Choco now uses TLS v1.2 - need to update Powershell as well

--- src/containers/w/packer/provisioners/chef/upload/prepare.ps1
+++ src/containers/w/packer/provisioners/chef/upload/prepare.ps1
@@ -1,5 +1,7 @@
 # choco could be not installed on VM during this step
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force;
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
 Write-Host "Reloading Path variable"
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 
